e1d821c2e15bdd57534b0dbd90d84a556072766a
sebasnop <sevalenciazapata@gmail.com>
2024-06-07 01:20:32 -0500

    refactor: Deleted unnecesary statements on Logoutbutton

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,11 +1,9 @@
 import { Button } from "@mui/material";
 import LogoutIcon from '@mui/icons-material/Logout';
 import { AppContextInterface, useAppContext } from "../context/AppContext";
-import { useState } from "react";
 
 function LogoutButton() {
   const signOutButtonText: string = "Sign out";
-  const [isLoading, setIsLoading] = useState<boolean>(false);
 
   const {
     loggedIn,
@@ -23,7 +21,6 @@ function LogoutButton() {
     endIcon={<LogoutIcon />} 
     sx={ loggedIn ? {display:"inline-flex"} : {display:"none"}}
     onClick={handlelogout}
-    disabled={isLoading}
     >
     { signOutButtonText }
   </Button>          
